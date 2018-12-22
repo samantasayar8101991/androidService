b0e029be816aff32515dbbd46d17ccf7df005298
Sayar Samanta <sayar.samanta@ladybirdweb.com>
2018-12-21 22:32:51 -0800

    sending data
    *sending data from service to activity wtih the help of broadcast
    receiver

--- app/src/main/java/com/example/sayarsamanta/androidservice/MainActivity.java
+++ app/src/main/java/com/example/sayarsamanta/androidservice/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.sayarsamanta.androidservice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private MyBroadcastReceiver myReceiver;
+    Button start, stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //On start button click start service
-        Button start = (Button) findViewById(R.id.button_start);
+        start = (Button) findViewById(R.id.button_start);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,12 +29,43 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //On stop button click start service
-        Button stop = (Button) findViewById(R.id.button_stop);
+        stop = (Button) findViewById(R.id.button_stop);
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 stopService(new Intent(getBaseContext(), MyService.class));
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        myReceiver = new MyBroadcastReceiver();
+        final IntentFilter intentFilter = new IntentFilter("YourAction");
+        LocalBroadcastManager.getInstance(this).registerReceiver(myReceiver, intentFilter);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        if (myReceiver != null)
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(myReceiver);
+        myReceiver = null;
+        super.onPause();
+    }
+
+    public class MyBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Here you have the received broadcast
+            // And if you added extras to the intent get them here too
+            // this needs some null checks
+            Bundle b = intent.getExtras();
+            String yourValue = b.getString("valueName");
+            stop.setText(yourValue);
+            double someDouble = b.getDouble("doubleName");
+            ///do something with someDouble
+        }
     }
 }
--- app/src/main/java/com/example/sayarsamanta/androidservice/MyService.java
+++ app/src/main/java/com/example/sayarsamanta/androidservice/MyService.java
@@ -2,18 +2,36 @@ package com.example.sayarsamanta.androidservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.Settings;
+import android.support.v4.content.LocalBroadcastManager;
 import android.widget.Toast;
 
 public class MyService extends Service {
+    private MediaPlayer player;
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
-        return super.onStartCommand(intent, flags, startId);
+//        player = MediaPlayer.create(this,Settings.System.DEFAULT_ALARM_ALERT_URI);
+//        player.setLooping(true);
+//        player.start();
+//        return START_STICKY;
+        Intent intent1 = new Intent("YourAction");
+        Bundle bundle = new Bundle();
+        for (int i=0;i<100;i++){
+            bundle.putString("valueName", "The value "+i);
+        }
+        //bundle.put... // put extras you want to pass with broadcast. This is optional
+
+        //bundle.putDouble("doubleName", someDouble);
+        intent1.putExtras(bundle);
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent1);
+        return START_STICKY;
     }
 
     @Override
